3a097d7baa5eda0e7af614df947103e84d80d6f9
Yohanes Tedy <yohanestedy52b@gmail.com>
2024-11-29 17:41:47 +0700

    add minor fix on footer

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="medium-text">
     <div class="footer clearfix mb-0 text-muted">
         <div class="float-start">
-            <p class="mb-0">2024 &copy; SiPeGi Selorejo | Created by <a href="https://github.com/yohanestedy"
+            <p class="mb-0">2024 &copy; SiPeGi Selorejo | Developed by <a href="https://github.com/yohanestedy"
                     target="_blank">Yohanes Tedy</a></p>
 
         </div>
