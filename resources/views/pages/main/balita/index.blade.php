735398b0567d5b2bae9fc4e0b6b7fb43389041c0
Yohanes Tedy <yohanestedy52b@gmail.com>
2024-12-16 10:06:01 +0700

    add detail biodata balita modal

--- resources/views/pages/main/balita/index.blade.php
+++ resources/views/pages/main/balita/index.blade.php
@@ -34,7 +34,7 @@
             <div class="row">
                 <div class="col-12 col-md-6 order-md-1 order-last">
                     <h3>Balita</h3>
-                    <p class="text-subtitle text-muted">Halaman daftar balita.</p>
+                    <p class="text-subtitle text-muted">Halaman daftar nama-nama balita aktif.</p>
                 </div>
 
             </div>
@@ -94,7 +94,41 @@
                                     <tr>
 
                                         <td style="text-align: center">
-                                            <a href="{{ route('balitaukur.detail', ['id' => $balita->id]) }}"
+
+                                            <button type="button" class="btn icon btn-info modal-btn btn-sm"
+                                                data-balita='@json($balita)' data-bs-toggle="tooltip"
+                                                data-bs-placement="top" data-bs-original-title="Lihat Detail"
+                                                style="border-radius: 8px; padding: .2rem .35rem; color:white;">
+                                                <i class="fa-solid fa-eye"></i>
+                                            </button>
+                                            <div class="btn-group">
+                                                <div class="dropdown">
+                                                    <button class="btn btn-primary btn-sm show"
+                                                        style="border-radius: 8px; padding: .2rem .35rem;" type="button"
+                                                        id="dropdownMenuButtonIcon" data-bs-toggle="dropdown"
+                                                        aria-haspopup="true" aria-expanded="true">
+                                                        <i class="fa-solid fa-ellipsis-vertical"></i>
+                                                    </button>
+
+                                                    <div class="dropdown-menu dropdown-menu-dark"
+                                                        aria-labelledby="dropdownMenuButtonIcon"
+                                                        style="position: absolute; inset: 0px auto auto 0px; margin: 0px; transform: translate3d(0px, 40px, 0px);"
+                                                        data-popper-placement="bottom-start">
+                                                        <a class="dropdown-item ps-3"
+                                                            href="{{ route('balitaukur.detail', ['id' => $balita->id]) }}"><i
+                                                                class="fa-regular fa-list-check me-1"></i>
+                                                            Riwayat Pengukuran</a>
+                                                        <a class="dropdown-item ps-3"
+                                                            href="{{ route('balita.edit', ['id' => $balita->id]) }}"><i
+                                                                class="fa-regular fa-pen-to-square me-1"></i>
+                                                            Ubah Biodata</a>
+
+
+                                                    </div>
+
+                                                </div>
+                                            </div>
+                                            {{-- <a href="{{ route('balitaukur.detail', ['id' => $balita->id]) }}"
                                                 class="btn icon btn-primary btn-sm" data-bs-toggle="tooltip"
                                                 data-bs-placement="top" data-bs-original-title="Riwayat Pengukuran"
                                                 style="border-radius: 8px; padding: .2rem .35rem;">
@@ -103,7 +137,7 @@
                                                 class="btn icon btn-success btn-sm" data-bs-toggle="tooltip"
                                                 data-bs-placement="top" data-bs-original-title="Edit"
                                                 style="border-radius: 8px; padding: .2rem .35rem;">
-                                                <i class="fa-regular fa-pen-to-square"></i></a>
+                                                <i class="fa-regular fa-pen-to-square"></i></a> --}}
 
                                             {{-- <form action="{{ route('balita.delete', ['id' => $balita->id]) }}"
                                                 method="POST" style="display: inline">
@@ -151,6 +185,131 @@
 
 
     </div>
+
+    {{-- MODAL HASIL PENGUKURAN --}}
+    <div class="modal fade" id="balitaModal" tabindex="-1" aria-labelledby="balitaModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header bg-primary">
+                    <h5 class="modal-title white" id="balitaModalLabel">Biodata Balita</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="row">
+                                <div class="col-7 col-md-6 pe-0">
+
+
+                                    <label class="medium-text">Nama Balita :</label><br>
+                                    <p class="text-start badge bg-light-secondary form-control-static fw-bold text-wrap"
+                                        id="balita_name">
+                                    </p><br>
+                                    <label class="medium-text">NIK Balita :</label><br>
+                                    <p class="text-start badge bg-light-secondary form-control-static fw-normal"
+                                        id="nik_balita">
+                                    </p><br>
+
+                                    <label class="medium-text">Tanggal Lahir :</label><br>
+                                    <p class="badge bg-light-secondary form-control-static fw-normal" id="tgl_lahir">
+                                    </p><br>
+                                    <label class="medium-text">Umur Saat Ini :</label><br>
+                                    <p class="badge bg-light-secondary form-control-static fw-normal" id="umur">
+                                    </p>
+                                    <br>
+
+
+
+
+                                </div>
+                                <div class="col-5 col-md-6">
+                                    <label class="medium-text">Jenis Kelamin :</label><br>
+                                    <p class="text-start badge bg-light-secondary form-control-static fw-normal"
+                                        id="gender">
+                                    </p><br>
+                                    <label class="medium-text">BB / TB Lahir :</label><br>
+                                    <p class="badge bg-light-secondary form-control-static fw-normal" id="bbtb_lahir">
+                                    </p><br>
+                                    <label class="medium-text">Posyandu :</label><br>
+                                    <p class="badge bg-light-secondary form-control-static fw-normal" id="posyandu">
+                                    </p><br>
+                                    <label class="medium-text">BPJS Balita :</label><br>
+                                    <p class="badge bg-light-secondary form-control-static fw-normal" id="bpjs">
+                                    </p><br>
+
+
+                                </div>
+                            </div>
+
+
+                        </div>
+                        <div class="col-md-12 mt-3">
+
+
+                            {{-- ROW KOLOM HEADER --}}
+                            <div class="row">
+                                <div class="col-9 col-md-9">
+                                    <label><strong>DATA ORANGTUA</strong></label>
+                                </div>
+
+                            </div>
+                            <hr class="my-2">
+
+                            <div class="row">
+                                <div class="col-7 col-md-6 pe-0">
+                                    <label class="medium-text">No KK :</label><br>
+                                    <p class="badge bg-light-secondary form-control-static fw-normal" id="kk">
+                                    </p><br>
+                                    <label class="medium-text">Nama Ibu :</label><br>
+                                    <p class="badge bg-light-secondary form-control-static fw-normal text-wrap"
+                                        id="name_ibu">
+                                    </p><br>
+                                    <label class="medium-text">NIK Ibu :</label><br>
+                                    <p class="badge bg-light-secondary form-control-static fw-normal" id="nik_ibu">
+                                    </p><br>
+                                    <label class="medium-text">Nama Ayah :</label><br>
+                                    <p class="badge bg-light-secondary form-control-static fw-normal text-wrap"
+                                        id="name_ayah">
+                                    </p><br>
+                                    <label class="medium-text">NIK Ayah :</label><br>
+                                    <p class="badge bg-light-secondary form-control-static fw-normal" id="nik_ayah">
+                                    </p><br>
+
+                                </div>
+                                <div class="col-5 col-md-6">
+
+
+
+                                    <label class="medium-text">No. Telp / WA :</label><br>
+                                    <p class="badge bg-light-secondary form-control-static fw-normal" id="telp">
+                                    </p><br>
+                                    <label class="medium-text">RT / RW :</label><br>
+                                    <p class="badge bg-light-secondary form-control-static fw-normal" id="rtrw">
+                                    </p><br>
+                                    <label class="medium-text">Dusun :</label><br>
+                                    <p class="badge bg-light-secondary form-control-static fw-normal" id="dusun">
+                                    </p><br>
+                                    <label class="medium-text">Alamat :</label><br>
+                                    <p class="badge bg-light-secondary form-control-static fw-normal text-wrap"
+                                        id="alamat">
+                                    </p><br>
+
+
+                                </div>
+                            </div>
+
+
+                        </div>
+
+                    </div>
+                </div>
+                {{-- <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+
+                </div> --}}
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('jsLibraries')
@@ -173,7 +332,7 @@
         }, false);
     </script>
 
-    {{-- Swal Alert --}}
+    {{-- Swal Alert Hapus --}}
     <script>
         $(document).ready(function() {
             // SweetAlert untuk konfirmasi penghapusan
@@ -209,42 +368,40 @@
         });
     </script>
 
-    {{-- Toast Sweatalert2 --}}
-    @if (session('successToast'))
-        <script>
-            const Toast = Swal.mixin({
-                toast: true,
-                position: "top-end",
-                showConfirmButton: false,
-                timer: 3000,
-                timerProgressBar: true,
-                didOpen: (toast) => {
-                    toast.onmouseenter = Swal.stopTimer;
-                    toast.onmouseleave = Swal.resumeTimer;
-                }
+    <script>
+        $(document).ready(function() {
+            // Delegasikan event click pada tombol modal-btn
+            $('#tableBalita').on('click', '.modal-btn', function() {
+                // Langsung parse objek data dari atribut
+                const balita = $(this).data('balita');
+                console.log(balita.orangtua.dusun.name);
+
+
+                // Isi modal dengan data dari objek balita dan ukur
+                $('#balita_name').text(balita.name);
+                $('#nik_balita').text(balita.nik ? balita.nik : '-');
+                $('#gender').text(balita.gender_display);
+                $('#tgl_lahir').text(balita.tgl_lahir_display);
+                $('#umur').text(balita.umur_display);
+                $('#bbtb_lahir').text(balita.bb_lahir + " kg" + " / " + balita.tb_lahir + " cm");
+                $('#posyandu').text(balita.posyandu.name);
+                $('#bpjs').text(balita.bpjs);
+
+                $('#kk').text(balita.orangtua.no_kk);
+                $('#name_ibu').text(balita.orangtua.name_ibu);
+                $('#nik_ibu').text(balita.orangtua.nik_ibu);
+                $('#name_ayah').text(balita.orangtua.name_ayah);
+                $('#nik_ayah').text(balita.orangtua.nik_ayah);
+                $('#telp').text(balita.orangtua.telp);
+                $('#alamat').text(balita.orangtua.alamat);
+                $('#rtrw').text(balita.orangtua.rt.rt + " / " + balita.orangtua.dusun.rw);
+                $('#dusun').text(balita.orangtua.dusun.name);
+
+
+
+                // Tampilkan modal
+                $('#balitaModal').modal('show');
             });
-            Toast.fire({
-                icon: "success",
-                title: "{{ session('successToast') }}"
-            });
-        </script>
-    @elseif (session('errorToast'))
-        <script>
-            const Toast = Swal.mixin({
-                toast: true,
-                position: "top-end",
-                showConfirmButton: false,
-                timer: 3000,
-                timerProgressBar: true,
-                didOpen: (toast) => {
-                    toast.onmouseenter = Swal.stopTimer;
-                    toast.onmouseleave = Swal.resumeTimer;
-                }
-            });
-            Toast.fire({
-                icon: "error",
-                title: "{{ session('errorToast') }}"
-            });
-        </script>
-    @endif
+        });
+    </script>
 @endsection
