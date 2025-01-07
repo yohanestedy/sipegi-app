eefc30e7bbaf680f5e6995c790d0c93eb5bed64e
Yohanes Tedy <yohanestedy52b@gmail.com>
2025-01-07 13:24:01 +0700

    fix logout button position

--- resources/views/include/sidebar.blade.php
+++ resources/views/include/sidebar.blade.php
@@ -30,7 +30,7 @@
         </div>
 
         {{-- SIDEBAR MENU --}}
-        <div class="sidebar-menu" style="margin-bottom: auto; min-height: 100vh">
+        <div class="sidebar-menu" style="margin-bottom: auto; height: 100vh">
             <ul class="menu" style="margin-top: -1rem;">
                 <li class="sidebar-title">Menu</li>
 
@@ -147,7 +147,7 @@
 
         <!-- Logout Button -->
         <div class="logout-container"
-            style="position: sticky; bottom: 0; height: 20vh; width: 100%; background-color: #f8f9fa; padding: 1rem;">
+            style="position: sticky; bottom: 0; height: 15vh; width: 100%; background-color: #f8f9fa; padding: 1rem;">
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: inline;">
                 @csrf
                 <div class="row">
