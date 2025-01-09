771f85b97531bf769ea148715236aeb8f4b4ac45
Yohanes Tedy <yohanestedy52b@gmail.com>
2025-01-09 19:58:52 +0700

    fix template for error page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>404 | SiPeGi Selorejo</title>
+    <title>404 - Sipegi</title>
     <link rel="icon" type="image/svg+xml" href="{{ asset('assets/static/images/logo/sipegi-favicon.svg') }}">
     <link rel="stylesheet" crossorigin href="{{ asset('assets/compiled/css/app.css') }}">
     <link rel="stylesheet" crossorigin href="{{ asset('assets/compiled/css/error.css') }}">
