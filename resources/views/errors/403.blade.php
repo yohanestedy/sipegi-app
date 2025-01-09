e3d9fddd440e070193ea7029ff30c19ad71fb7b4
Yohanes Tedy <yohanestedy52b@gmail.com>
2025-01-09 20:05:43 +0700

    fix minor template for error page

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -19,9 +19,10 @@
             <div class="col-md-8 col-12 offset-md-2">
                 <div class="text-center">
                     <img class="img-error" src="{{ asset('assets/compiled/svg/error-403.svg') }}" alt="Forbidden">
-                    <h1 class="error-title">Forbidden</h1>
-                    <p class="fs-5 text-gray-600">You are unauthorized to see this page.</p>
-                    <a href="{{ route('home') }}" class="btn btn-lg btn-outline-primary mt-3">Go Home</a>
+                    <h1 class="error-title">Akses Ditolak</h1>
+                    <p class="fs-5 text-gray-600">Maaf, Anda tidak memiliki izin untuk mengakses halaman ini.</p>
+                    <a href="{{ route('home') }}" class="btn btn-lg btn-outline-primary mt-3">Kembali ke
+                        Dashboard</a>
                 </div>
             </div>
         </div>
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -20,9 +20,10 @@
                 <div class="text-center">
                     <img class="img-error" src="{{ asset('assets/compiled/svg/error-500.svg') }}" alt="System Error">
                     <h1 class="error-title">System Error</h1>
-                    <p class="fs-5 text-gray-600">The website is currently unaivailable. Try again later or contact the
-                        developer.</p>
-                    <a href="{{ route('home') }}" class="btn btn-lg btn-outline-primary mt-3">Go Home</a>
+                    <p class="fs-5 text-gray-600">Maaf, saat ini website kami sedang mengalami gangguan. Silakan coba
+                        lagi nanti atau hubungi pengembang jika Anda memerlukan bantuan.</p>
+                    <a href="{{ route('home') }}" class="btn btn-lg btn-outline-primary mt-3">Kembali ke
+                        Dashboard</a>
                 </div>
             </div>
         </div>
