1c11efc97e329f0bad3aba5aa0968cdd2cef14e5
Yohanes Tedy <yohanestedy52b@gmail.com>
2025-01-09 20:00:24 +0700

    fix minor template for error page

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -22,7 +22,7 @@
                     <h1 class="error-title">System Error</h1>
                     <p class="fs-5 text-gray-600">The website is currently unaivailable. Try again later or contact the
                         developer.</p>
-                    <a href="{{ route('home') }} class="btn btn-lg btn-outline-primary mt-3">Go Home</a>
+                    <a href="{{ route('home') }}" class="btn btn-lg btn-outline-primary mt-3">Go Home</a>
                 </div>
             </div>
         </div>
