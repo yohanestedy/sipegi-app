92786381bdae577ed1ead64d495bed8a7871369c
yohanestedy <yohanestedy52b@gmail.com>
2025-02-05 11:17:36 +0700

    style: change hint information for table

--- resources/views/pages/main/balita/index.blade.php
+++ resources/views/pages/main/balita/index.blade.php
@@ -55,8 +55,9 @@
 
                     <div class="mb-2">
                         <p class="d-block d-xl-none"
-                            style="font-size: 0.85rem; color: #9d9e9f; font-weight: 500; margin-top: 8px; font-style: italic; text-align: left;">
-                            *Geser ke samping untuk melihat data
+                            style="font-size: 0.75rem; color: #c2c2c2; font-style:italic; font-weight: 500; margin-top: 8px;  text-align: left;">
+                            *Klik tombol <i class="fa-solid fa-magnifying-glass"></i> atau geser ke <i
+                                class="fa-solid fa-arrow-right-long"></i> untuk melihat data
                         </p>
                     </div>
                     <div class="table-responsive datatable-minimal">
